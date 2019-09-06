9c6ed407555c48a6e7da40a3702760bfedc6b460
Opilane TTHK <opilane@tthk.ee>
2019-09-06 17:03:55 +0300

    My first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C#SucksLikeHell");
+            Console.WriteLine("Why do you hate c#?");
+            string cSharpSucksLikeHell;
+            cSharpSucksLikeHell = Console.ReadLine();
+            Console.WriteLine(" Hello " + cSharpSucksLikeHell);
+            Console.ReadLine();
+        }
+    }
+}
